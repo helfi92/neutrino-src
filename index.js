340375a36b57f3039cbaf8c0816428a61ce74b5f
helfi92 <helfi92@gmail.com>
2017-01-18 08:37:35 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import './custom.css';
+import './custom-2.css';
+
+import './react-app';
+// import './web-app';
---
+++ react-app/actions.js
@@ -0,0 +1,11 @@
+export const increaseCounter = () => {
+  return {
+    type: 'INCREASE_COUNTER',
+  };
+};
+
+export const decreaseCounter = () => {
+  return {
+    type: 'DECREASE_COUNTER',
+  };
+};
