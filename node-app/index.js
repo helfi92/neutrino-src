8f3efc3d250166c463c8b36d44bf3d5603b5c836
helfi92 <helfi92@gmail.com>
2017-02-02 19:03:02 -0500

    Add node setup

--- node-app/index.js
+++ node-app/index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const app = express();
 const path = require('path');
 
-path.join(__dirname);
+app.use(express.static(path.join(process.cwd(), 'src', 'node-app', 'public')));
+
+app.get('/', (req, res) => {
+  res.sendfile(path.resolve(__dirname, 'src', 'node-app', 'public', 'index.html'));
+});
+
+app.listen(8080);
